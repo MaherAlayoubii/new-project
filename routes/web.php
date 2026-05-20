02c711bf869b5cae83a228960bf86a9538da9e07
MaherAlayoubii <maherelayoubi@gmail.com>
2026-05-20 17:12:01 +0300

    delete , update , Query Builder

--- routes/web.php
+++ routes/web.php
@@ -33,14 +33,34 @@ Route::post('/about', function () {
 });
 
 Route::get('/tasks', function () {
-    return view('tasks');
+    $tasks = DB::table('tasks')->get();
+    return view('tasks' , compact('tasks'));
 });
 
-Route::post('create', function () {
+Route::post('/tasks', function () {
     $tasks_name = $_POST['name'];
     DB::table('tasks')->insert([
         'name' => $tasks_name,
-       
+
     ]);
-    return view('tasks');
+    return redirect()->back();
+});
+
+Route::post('/tasks/delete/{id}', function ($id) {
+    DB::table('tasks')->where('id', $id)->delete();
+    return redirect()->back();
+});
+
+Route::post('/tasks/edit/{id}', function ($id) {
+    $task = DB::table('tasks')->where('id', $id)->first();
+    $tasks = DB::table('tasks')->get();
+    return view('tasks', compact('task', 'tasks'));
+});
+
+Route::post('update', function () {
+    $id = $_POST['id'];
+    DB::table('tasks')->where('id','=', $id)->update([
+        'name' => $_POST['name'],
+    ]);
+    return redirect('tasks');
 });
